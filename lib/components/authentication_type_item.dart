affbad08890e4153ecb8b52d3287282bb39f82f2
zeeshanmalik0110 <zeeshandevsinn@gmail.com>
2024-10-06 13:13:59 +0500

    done all

---
+++ lib/components/authentication_type_item.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'package:mobile/common/app_text_styles.dart';
+
+class AuthenticationTypeItem extends StatelessWidget {
+  const AuthenticationTypeItem({
+    Key? key,
+    required this.text,
+    required this.isActive,
+    required this.onTap,
+  }) : super(key: key);
+
+  final String text;
+  final bool isActive;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        
+        padding:
+            const EdgeInsets.only(top: 15, bottom: 15, left: 20, right: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          gradient: isActive
+              ? const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.blue,
+                    Colors.black,
+                  ],
+                )
+              : null,
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: AppTextStyles.poppinsMedium().copyWith(
+                  color: isActive ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+        ),
+      ),
+    ); 
+  }
+}
---
+++ lib/components/authentication_type_view.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:mobile/common/app_colors.dart';
+import 'package:mobile/components/authentication_type_item.dart';
+
+class AuthenticationTypeView extends StatelessWidget {
+  const AuthenticationTypeView({
+    Key? key,
+    required this.onTap,
+    required this.isActive,
+  }) : super(key: key);
+
+  final bool isActive;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+   var size = MediaQuery.of(context).size;
+    return Container(
+     width: size.width*0.7,
+      decoration: BoxDecoration(
+        color: Color(0xffdcdedc),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AuthenticationTypeItem(
+            text: 'Personal',
+            isActive: !isActive,
+            onTap: onTap,
+          ),
+          AuthenticationTypeItem(
+            text: 'Company',
+            isActive: isActive,
+            onTap: onTap,
+          ),
+        ],
+      ),
+    );
+  }
+}
